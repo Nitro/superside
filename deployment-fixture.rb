d04b3bd98877486198832d1e921c2a2c662302b3
Karl Matthias <karl.matthias@gonitro.com>
2016-07-19 11:37:10 +0100

    Ruby script and JSON to simulate a few deploys.

---
+++ deployment-fixture.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'excon'
+
+connection = Excon.new('http://localhost:7778/update')
+
+state_blob = {
+  'State' => {
+    'ClusterName' => 'nitro-dev',
+    'Hostname' => 'awesome-host'
+  }
+}
+
+data = JSON.parse(File.read('deployment-sample.json'))
+
+data.each do |event|
+  connection.post(
+    body: state_blob.merge({
+        'ChangeEvent' => {
+          'Service': event['Event']['Service']
+        }
+      }).to_json,
+    headers: { 'Content-type': 'application/json' },
+    persistent: true
+  )
+end
