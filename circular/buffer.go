9e8f713e9a6fac66d356b70202dceaa1deeba82a
Karl Matthias <karl.matthias@gonitro.com>
2016-07-19 10:20:36 +0100

    Only need one pointer to track this!

--- circular/buffer.go
+++ circular/buffer.go
@@ -9,23 +9,19 @@ import (
 
 type Buffer struct {
 	changes     *ring.Ring
-	currentNode *ring.Ring
 }
 
 // Return a new, properly configured circular buffer
 func NewBuffer(size int) *Buffer {
 	newRing := ring.New(size)
 
-	return &Buffer{
-		changes:     newRing,
-		currentNode: newRing,
-	}
+	return &Buffer{changes: newRing}
 }
 
 // Get all the items from the buffer that have a value, return as linear slice
 func (b *Buffer) List() []notification.Notification {
 	var changeHistory []notification.Notification
-	b.changes.Do(func(evt interface{}) {
+	b.changes.Do(func(evt interface{}) { // Start from oldest node
 		if evt == nil {
 			return
 		}
@@ -38,6 +34,6 @@ func (b *Buffer) List() []notification.Notification {
 }
 
 func (b *Buffer) Insert(evt catalog.StateChangedEvent) {
-	b.currentNode.Value = evt
-	b.currentNode = b.currentNode.Next()
+	b.changes.Value = evt
+	b.changes = b.changes.Next()
 }
