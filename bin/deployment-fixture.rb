33f6d98ad41a60f2fd010f889b51dca9485dcde9
Karl Matthias <karl.matthias@gonitro.com>
2016-07-21 13:05:21 +0100

    More reasonable looking deployment with sleeps.

--- bin/deployment-fixture.rb
+++ bin/deployment-fixture.rb
@@ -7,6 +7,7 @@ require 'trollop'
 opts = Trollop::options do
   opt :url,     'The URL to post updates to', default: 'http://localhost:7778/api/update'
   opt :fixture, 'The fixture to post', default: '../../fixtures/deployment-sample.json'
+  opt :sleep,   'The number or decimal seconds to sleep between events', default: 0.3
 end
 
 connection = Excon.new(opts[:url])
@@ -22,6 +23,7 @@ fixture = File.expand_path(opts[:fixture], __FILE__)
 data = JSON.parse(File.read(fixture))
 
 data.each do |event|
+  sleep opts[:sleep]
   event['Event']['Service']['Hostname'] = %w{ host1 host2 host3 host4 }.shuffle.first
 
   data =  JSON.pretty_generate(state_blob.merge({
