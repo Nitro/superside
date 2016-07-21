948c90db72e753d4e6458c51c27a39e0d8420ea7
Karl Matthias <karl.matthias@gonitro.com>
2016-07-21 16:20:02 +0100

    Working UI with warnings visible for deployed versions.

--- public/app/views/events.js
+++ public/app/views/events.js
@@ -23,7 +23,33 @@
 			onError: function(event) {
 				console.log(event)
 			}
-		})
+		});
+
+		// Compare versions. If they don't match, warning. If they don't match
+		// and also are more than a day apart, then danger/red.
+		self.getVersionMatchStatus = function(svcName) {
+			var clusters = stateService.services[svcName];
+			var groups = _.groupBy(clusters, function(cluster) {
+				return cluster.Version
+			})
+
+			// All versions matched, good to go
+			if (Object.keys(groups).length == 1) {
+				return '';
+			}
+
+			var sorted = _.sortBy(clusters, 'EndTime')
+
+			var oneDay = 3600*24*1000;
+			var firstDate = new Date(sorted[0].EndTime).getTime();
+			var lastDate = new Date(sorted[sorted.length-1].EndTime).getTime();
+
+			if (lastDate > firstDate + oneDay) {
+				return 'bg-danger';
+			}
+
+			return 'bg-warning';
+		};
 
     }
 })(angular);
