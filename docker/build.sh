ce26474e1afb65b4a0f6d62f79e49c6d017424c7
Karl Matthias <karl.matthias@gonitro.com>
2017-03-21 16:25:18 +0000

    Tag and push Docker images

--- docker/build.sh
+++ docker/build.sh
@@ -10,3 +10,9 @@ test -f ../superside.toml || die "Missing superside.toml file!"
 
 cd ../public && npm install
 cd .. && docker build -f docker/Dockerfile -t superside . || die "Failed to build"
+docker tag superside gonitro/superside:latest
+docker push gonitro/superside:latest
+
+TAG=`git rev-parse --short HEAD`
+docker tag superside gonitro/superside:${TAG}
+docker push gonitro/superside:${TAG}
